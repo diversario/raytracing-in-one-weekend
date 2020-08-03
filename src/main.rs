25dd76c3387ea21a9cfe3c4adadc36ed727b0f15
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2020-08-03 21:59:10 +0200

    aw shit it's a sky

--- src/main.rs
+++ src/main.rs
@@ -11,27 +11,23 @@ mod ray;
 use vec3::Vec3;
 use point3::Point3;
 use color::Color;
+use ray::Ray;
 
 fn main() {
     // Image
     let aspect_ratio = 16.0 / 9.0;
     let image_width = 400.0;
-    let image_height = (image_width / aspect_ratio) as i64;
+    let image_height = (image_width / aspect_ratio) as i64 as f64;
 
-        // Camera
-
+    // Camera
     let viewport_height = 2.0;
     let viewport_width = aspect_ratio * viewport_height;
     let focal_length = 1.0;
 
     let origin = Point3::new(0.0, 0.0, 0.0);
     let horizontal = Vec3::new(viewport_width, 0.0, 0.0);
-    let vertical = vec3(0, viewport_height, 0);
-    let lower_left_corner = origin - horizontal/2 - vertical/2 - vec3(0, 0, focal_length);
-
-
-    let width = 256;
-    let height = 256;
+    let vertical = Vec3::new(0.0, viewport_height, 0.0);
+    let lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - Vec3::new(0.0, 0.0, focal_length);
 
     let mut file : fs::File;
 
@@ -40,19 +36,19 @@ fn main() {
       Err(error) => panic!(error.to_string()),
     };
 
-    writeln!(file, "P3\n{} {}\n255\n", width, height);
+    writeln!(file, "P3\n{} {}\n255\n", image_width, image_height);
 
-    for y in (0..height).rev() {
-      print!("\rRendered {}%", (((height - y) as f64/ height as f64) * 100.0) as i64); io::stdout().flush().unwrap();
+    for y in (0..image_height as i64).rev() {
+      print!("\rRendered {}%", (((image_height - y as f64) as f64/ image_height as f64) * 100.0) as i64); io::stdout().flush().unwrap();
 
-      for x in 0..width {
-        let color = color::Color::new(
-          x as f64/(width-1) as f64,
-          y as f64/(height-1) as f64,
-          0.6,
-        );
+      for x in 0..image_width as i64 {
+        let u = x as f64/(image_width-1.0) as f64;
+        let v = y as f64/(image_height-1.0) as f64;
 
-        writeln!(file, "{}\n", color::write_color(color));
+        let ray = Ray::new(origin, lower_left_corner + horizontal * u + vertical * v - origin);
+        let pixel_color = ray::ray_color(&ray);
+
+        writeln!(file, "{}\n", color::write_color(pixel_color));
       };
     };
 
--- src/ray.rs
+++ src/ray.rs
@@ -1,5 +1,6 @@
 use crate::point3::Point3;
 use crate::vec3::Vec3;
+use crate::color::Color;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Ray {
@@ -26,4 +27,10 @@ impl Ray {
   pub fn at(self, t: f64) -> Point3 {
     self.orig + self.dir * t
   }
+}
+
+pub fn ray_color(r: &Ray) -> Color {
+    let unit_direction = Vec3::unit_vector(r.direction());
+    let t = 0.5*(unit_direction.y() + 1.0);
+    return Color::new(1.0, 1.0, 1.0) * (1.0-t) + Color::new(0.5, 0.7, 1.0) * t;
 }
