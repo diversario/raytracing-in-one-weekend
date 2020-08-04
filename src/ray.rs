572b8dbaf4a162af277574fadca3f2826bd1027b
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2020-08-04 21:06:31 +0200

    normals!!

--- src/ray.rs
+++ src/ray.rs
@@ -1,6 +1,6 @@
+use crate::color::Color;
 use crate::point3::Point3;
 use crate::vec3::Vec3;
-use crate::color::Color;
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct Ray {
@@ -9,7 +9,7 @@ pub struct Ray {
 }
 
 impl Ray {
-  pub fn new(origin :Point3, direction: Vec3) -> Ray {
+  pub fn new(origin: Point3, direction: Vec3) -> Ray {
     Ray {
       orig: origin,
       dir: direction,
@@ -30,20 +30,29 @@ impl Ray {
 }
 
 pub fn ray_color(r: &Ray) -> Color {
-      if hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r) {
-        return Color::new(1.0, 0.0, 0.0);
-      }
+  let mut t = hit_sphere(&Point3::new(0.0, 0.0, -1.0), 0.5, r);
 
-    let unit_direction = Vec3::unit_vector(r.direction());
-    let t = 0.5*(unit_direction.y() + 1.0);
-    return Color::new(1.0, 1.0, 1.0) * (1.0-t) + Color::new(0.5, 0.7, 1.0) * t;
+  if t > 0.0 {
+    let normal = Vec3::unit_vector(r.at(t) - Vec3::new(0.0, 0.0, -1.0));
+    return Color::new(normal.x() + 1.0, normal.y() + 1.0, normal.z() + 1.0) * 0.5;
+  }
+
+  let unit_direction = Vec3::unit_vector(r.direction());
+
+  t = 0.5 * (unit_direction.y() + 1.0);
+
+  return Color::new(1.0, 1.0, 1.0) * (1.0 - t) + Color::new(0.5, 0.7, 1.0) * t;
 }
 
-pub fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> bool {
-    let oc = r.origin() - *center;
-    let a = Vec3::dot(r.direction(), r.direction());
-    let b = 2.0 * Vec3::dot(oc, r.direction());
-    let c = Vec3::dot(oc, oc) - radius*radius;
-    let discriminant = b * b - 4.0*a*c;
-    discriminant > 0.0
+pub fn hit_sphere(center: &Point3, radius: f64, r: &Ray) -> f64 {
+  let oc = r.origin() - *center;
+  let a = r.direction().length_squared();
+  let half_b = Vec3::dot(oc, r.direction());
+  let c = oc.length_squared() - radius * radius;
+  let discriminant = half_b * half_b - a * c;
+  if discriminant < 0.0 {
+    -1.0
+  } else {
+    (-half_b - discriminant.sqrt()) / a
+  }
 }
